1f2e14db832f9a696d88ef795d9aac19edeaa453
Augustin Mulliez <mulliez.augustin@gmail.com>
2017-09-21 15:08:09 +0200

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home', to: 'pages#home'
+  get 'contact', to: 'pages#contact'
+  root to: 'pages#home'
 end
