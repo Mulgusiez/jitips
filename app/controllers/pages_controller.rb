d984e8dd14adc602bd4406127ff3fa6435d9165e
Augustin Mulliez <mulliez.augustin@gmail.com>
2017-10-12 13:20:18 +0200

    looking for a CTO set up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,9 @@ class PagesController < ApplicationController
   end
 
   def cherche_un_dev
+    redirect_to looking_for_a_CTO_path
+  end
+
+  def looking_for_a_CTO
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home', to: 'pages#home'
   get 'cherche_un_dev', to: 'pages#cherche_un_dev'
+  get 'looking_for_a_CTO', to: 'pages#looking_for_a_CTO'
   root to: 'pages#home'
 end
